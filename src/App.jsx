fbef96440db13a5c99660e83d6c21c440af53b93
Your Name <solankijignesh4611@gmail.com>
2025-04-03 15:55:11 +0530

    form handling

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,33 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const App = () => {
-  const submitHandler = (e)=>{
-    e.preventDefault()
-    console.log("submited")
-  } 
+  const [state, setState] = useState("");
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log(state);
+    setState('') };
   return (
     <div>
-      <form onSubmit={(e)=>{
-        submitHandler(e);
-      }}>  
-        <input className='px-4 py-3 text-xl m-5'  type="text" placeholder='Enter Your name '/>
-        <button className='px-4 py-3 m-5 text-xl font-semibold bg-emerald-500 rounded '> Submit</button>
+      <form
+        onSubmit={(e) => {
+          submitHandler(e);
+        }}
+      >
+        <input
+          value={state}
+          onChange={(e) => {
+            setState(e.target.value);
+          }}
+          className="px-4 py-3 text-xl m-5"
+          type="text"
+          placeholder="Enter Your name "
+        />
+        <button className="px-4 py-3 m-5 text-xl font-semibold bg-emerald-500 rounded ">
+          Submit
+        </button>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default App
- 
+export default App;
