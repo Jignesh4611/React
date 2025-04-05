91fc7e928f646673963fbfbf4cb1ffa4d8063ec2
Your Name <solankijignesh4611@gmail.com>
2025-04-05 11:54:30 +0530

    footer made

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,13 @@
-import React, { useState } from "react";
-
+import React from "react";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 const App = () => {
-  const [state, setState] = useState("");
-  const submitHandler = (e) => {
-    e.preventDefault();
-    console.log(state);
-    setState('') };
   return (
-    <div>
-      <form
-        onSubmit={(e) => {
-          submitHandler(e);
-        }}
-      >
-        <input
-          value={state}
-          onChange={(e) => {
-            setState(e.target.value);
-          }}
-          className="px-4 py-3 text-xl m-5"
-          type="text"
-          placeholder="Enter Your name "
-        />
-        <button className="px-4 py-3 m-5 text-xl font-semibold bg-emerald-500 rounded ">
-          Submit
-        </button>
-      </form>
-    </div>
+    <>
+    <Navbar/>
+  
+    <Footer/>
+    </>
   );
 };
 
